60472c582813dc3d4d36dd5320cc9125cb426162
major1balaz525 <major.balazs@telekom.hu>
2022-03-28 10:46:52 +0200

    fix(#CSAK-0000): fix increment service test and add Dockerfile

--- src/test/kotlin/com/example/demo/service/BalanceServiceTest.kt
+++ src/test/kotlin/com/example/demo/service/BalanceServiceTest.kt
@@ -10,14 +10,14 @@ class BalanceServiceTest {
     private val balanceService: BalanceService = BalanceService()
 
     @Test
-    fun testIncrease() {
+    fun testDeposit() {
         // given
         val persistentAccount: PersistentAccount = PersistentAccount("1", "John", 1500.0)
         val amount: Double = 100.0
         // when
         val newBalance: Double = balanceService.increase(persistentAccount, amount)
         // then
-        Assertions.assertEquals(1600.5, newBalance)
+        Assertions.assertEquals(1600.0, newBalance)
     }
 
     @Test
